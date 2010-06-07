b85130ba147a0f92497c61f2138b1765d6de82ec
Charlie Melbye <charlie@yourwiki.net>
2010-06-06 20:31:50 -0500

    allow specifying where the bot hugs the user

--- modules/hug.php
+++ modules/hug.php
@@ -1,7 +1,12 @@
 <?php
 if(command('hug')) {
 	$person = $msg_args[1];
-	$multibot->write($network_name, "PRIVMSG " . $args[2] . " :" . chr(1) . "ACTION bounces into " . $person . "'s lap and huggles and *hugs*" . chr(1));
+	if($msg_args[2] == "in" && $msg_args[3][0] == "#") {
+	  $channel = $msg_args[3];
+	} else {
+	  $channel = $args[2];
+	}
+	$multibot->write($network_name, "PRIVMSG " . $channel . " :" . chr(1) . "ACTION bounces into " . $person . "'s lap and huggles and *hugs*" . chr(1));
 }
 
 Hooks::add('hug', 'hug');
